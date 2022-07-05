9686b70b44ff512f1644f1e7171022b806ce25e5
Steesha <jsteesha@gmail.com>
2022-07-05 19:55:37 +0800

    项目大重构

---
+++ FluxInjector/FluxInjector.cpp
@@ -0,0 +1,11 @@
+﻿// FluxInjector.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "[Flux] for miniwordld" << endl;
+    return 0;
+}
