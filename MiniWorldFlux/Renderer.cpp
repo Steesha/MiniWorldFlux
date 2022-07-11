582cb0d4f368e6ac2f8ab633f2dc4365e7c0c370
Steesha <jsteesha@gmail.com>
2022-07-11 22:04:14 +0800

    去掉不必要的保护macro

--- MiniWorldFlux/Renderer.cpp
+++ MiniWorldFlux/Renderer.cpp
@@ -18,7 +18,6 @@ namespace Renderer {
 	IDirect3DTexture9* playerOverlayTex = nullptr;
 
 	void initImGui(HWND hwnd, IDirect3DDevice9* device) {
-		VM_LION_WHITE_START
 		IMGUI_CHECKVERSION();
 		ImGui::CreateContext();
 		ImGuiIO& io = ImGui::GetIO();
@@ -42,11 +41,9 @@ namespace Renderer {
 
 		ImGui_ImplWin32_Init(hwnd);
 		ImGui_ImplDX9_Init(device);
-		VM_LION_WHITE_END
 	}
 
 	bool hookDx9(HWND hwnd) {
-		VM_LION_WHITE_START
 		// ´´½¨D3D
 		IDirect3D9* direct3D9 = Direct3DCreate9(D3D_SDK_VERSION);
 		if (direct3D9 == nullptr) {
@@ -86,7 +83,6 @@ namespace Renderer {
 		direct3DDevice9->Release();
 
 		return true;
-		VM_LION_WHITE_END
 	}
 
 	HRESULT createTexture(IDirect3DDevice9* pDevice, IDirect3DTexture9** ppD3Dtex, DWORD colour32) {
